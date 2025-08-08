db2d74f847c8d88908c6adf0bf7f8e4ea40e1cc0
ola <olanazmy@gmail.com>
2025-08-09 01:37:41 +0300

    DbContext and Villa Model Added

---
+++ MagicVilla.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
