9534a7d573ef0f3175ead9c98d196df70c4e78bf
ola <olanazmy@gmail.com>
2025-08-09 02:05:58 +0300

    Ctor configured

--- MagicVilla.API/Data/ApplicationDbContext.cs
+++ MagicVilla.API/Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace MagicVilla.API.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Villa> Villas { get; set; }
     }
 }
