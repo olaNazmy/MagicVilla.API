232c2e6293b30914fee50dd79b98879c18ff0781
ola <olanazmy@gmail.com>
2025-08-13 13:02:19 +0300

    Separate DTO for each process

---
+++ MagicVilla.API/Models/DTO/VillaCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.API.Models.DTO
+{
+    public class VillaCreateDTO
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
---
+++ MagicVilla.API/Models/DTO/VillaUpdateDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.API.Models.DTO
+{
+    public class VillaUpdateDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public int Sqft { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
