139d9302db51a26c91b542738fff6fc506a67181
ola <olanazmy@gmail.com>
2025-09-09 21:11:28 +0300

    VillaNumber model added with it's DTOs

---
+++ MagicVilla.API/Models/DTO/VillaNumberCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla.API.Models.DTO
+{
+    public class VillaNumberCreateDTO
+    {
+        [Required]
+        public int VillNo { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
---
+++ MagicVilla.API/Models/VillaNumber.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla.API.Models
+{
+    public class VillaNumber
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int VillNo { get; set; }
+        public string SpecialDetails { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
